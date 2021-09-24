fa167f4953a27ad8adb912d72d02c23ee55edfa1
David De Vargas <dvccueter@gmail.com>
2021-09-24 08:18:32 -0500

    feat: building the ProductCard component

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,9 @@
     </div>
   </header>
 
-  <router-view :inventory="inventory" :addToCart="addToCart" />
+  <router-view
+    :inventory="inventory"
+    :addToCart="addToCart" />
 
   <Sidebar
     v-if="showSidebar"
@@ -52,10 +54,9 @@ export default {
     }
   },
   methods: {
-    addToCart (name, index) {
+    addToCart (name, quantity) {
       if (!this.cart[name]) this.cart[name] = 0
-      this.cart[name] += this.inventory[index].quantity
-      this.inventory[index].quantity = 0
+      this.cart[name] += quantity
     },
     toggleSidebar () {
       this.showSidebar = !this.showSidebar
